dcdd8341344d98dbbab1cbc8b2c090504af1c707
ArcaneSanity <arcanesanity@gmail.com>
2012-09-04 02:32:19 +0300

    add rune ids

--- Runecrafting/ReverseNatures/util/Constants.java
+++ Runecrafting/ReverseNatures/util/Constants.java
@@ -10,8 +10,12 @@ public class Constants {
 	public static final int NATURE_RUINS = 2460;
 	public static final int NATURE_ALTAR = 2486;
 	public static final int BANKER = 2617;
+	public static final int AIR_RUNE = 556;
 	public static final int NATURE_RUNE = 561;
-	public static final int ESSENCE = 7936;
+	public static final int LAW_RUNE = 563;
+	public static final int COSMIC_RUNE = 564;
+	public static final int ASTRAL_RUNE = 9075;
+	public static final int PURE_ESSENCE = 7936;	
 	public static final int[] DEGRADED_POUCHES = { 5515, 5513, 5511 };
 	
 	public static final Tile[] TO_RING = {new Tile(0000,0000,0), new Tile(0000,0000,0)};	
